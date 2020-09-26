5d68e0a960fea97575cc441b9531cdcc43352643
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-09-26 23:35:21 +0300

    Add basic navbar and move App.js and App.css to the root of the project

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => (
+    <div className="navbar">
+	<ul>
+	    <li><a href="#">Home</a></li>
+	</ul>
+    </div>
+);
+
+export default Navbar;
